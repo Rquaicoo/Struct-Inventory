a3aa948f09cca21c83e1297da7bb4a2f0c257181
Rquaicoo <russellquaicoo1@gmail.com>
2022-08-30 23:30:42 +0000

    fields for vendor models

--- src/main/java/models/Vendor.java
+++ src/main/java/models/Vendor.java
@@ -4,13 +4,14 @@ public class Vendor {
     public String username;
     public String password;
     public String email;
+    public String name;
 
-    public  Vendor(String username, String password) {
+    public  Vendor(String username, String password, String email, String name) {
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.name = name;
+
     }
 
-    public void login() {
-
-    }
 }
