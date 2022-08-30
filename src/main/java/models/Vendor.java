950b65eb18117155314808c7fd7af0d333863e02
Rquaicoo <russellquaicoo1@gmail.com>
2022-08-30 15:31:45 +0000

    classes for models

---
+++ src/main/java/models/Vendor.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Vendor {
+    public String username;
+    public String password;
+    public String email;
+
+    public  Vendor(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void login() {
+
+    }
+}
